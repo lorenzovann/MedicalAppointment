0c5791124e8695ca6c22eefa1096ca611dedfccd
walter lorenzo vannoni martinez <lorenzovannoni87@gmail.com>
2024-09-28 22:37:30 -0400

    Estructura del Proyecto

---
+++ MedicalAppointment.Domain/Entities/Configuration/Appointments.cs
@@ -0,0 +1,16 @@
+﻿
+
+using MedicalAppointment.Domain.Base;
+
+namespace MedicalAppointment.Domain.Entities.Configuration
+{
+    public class Appointments : BaseEntity
+    {
+        public int AppointmentID { get; set; }
+        public int PatientID { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public int StatusID { get; set; }
+       
+    }
+}
