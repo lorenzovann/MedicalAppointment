fe131c7f90dfe273c9fb43ece31c51080e3d0768
walter lorenzo vannoni martinez <lorenzovannoni87@gmail.com>
2024-09-30 12:07:25 -0400

    Estructura del proyecto Acualizada

--- MedicalAppointment.Domain/Result/OperationResult.cs
+++ MedicalAppointment.Domain/Result/OperationResult.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace MedicalAppointment.Domain.Result
 {
     public class OperationResult
     {
+        public string Message { get; set; }
+        public bool Success { get; set; }
+        public dynamic Data { get; set; }
 
     }
 }
