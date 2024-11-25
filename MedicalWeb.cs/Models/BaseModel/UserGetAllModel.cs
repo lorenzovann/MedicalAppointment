28d145c378344ccaa7253f0a11d4c31fd020223a
Isma Segura <126912611+Isma-123@users.noreply.github.com>
2024-11-25 00:05:17 -0400

    Delete MedicalWeb.cs directory

--- MedicalWeb.cs/Models/BaseModel/UserGetAllModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using MedicalAppointment.Persistance.Model;
-
-namespace MedicalWeb.cs.Models.BaseModel
-{
-    public class UserGetAllModel : BaseApiResponse
-    {
-        public List<UserModel> data { get; set; }
-    }
-}
